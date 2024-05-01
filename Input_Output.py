d1f836e74204157f628607a472f78c4d4566cdec
Sisriksha Dhakal <shreedharpandey@Binitas-MacBook-Air.local>
2024-05-01 14:57:07 -0700

    problem 2

---
+++ Input_Output.py
@@ -0,0 +1,12 @@
+# problem 2
+#input: nick
+#output: greetings nick
+#problem: input name and outout greetings with name
+#solution: assigning the name of input in string data type  as a variable and  getting output as a string
+
+def my_function():
+    name = input("enter your name: ")
+    print("Greetings!",name)
+my_function()  
+  
+
