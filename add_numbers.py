7e0d7160c14bad340a0c53ab9f84ab6ddb6ab1b4
Sisriksha Dhakal <shreedharpandey@Binitas-MacBook-Air.local>
2024-05-02 09:52:50 -0700

    problem 3

---
+++ add_numbers.py
@@ -0,0 +1,9 @@
+#adding two numbers
+#input:assigning two variables value containing integer
+#problem:adding numbers
+#solution:adding two integers 
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+sum = a + b
+print(sum)
+
