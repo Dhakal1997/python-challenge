c2e9fed85d0ae5308c87cb074c359117882b0e29
Sisriksha Dhakal <shreedharpandey@Binitas-MacBook-Air.local>
2024-05-02 10:18:23 -0700

    problem 4

--- area_circle.py
+++ area_circle.py
@@ -1,4 +1,10 @@
-#input:
-#output:
-#problems:
-#solution:
+#input: 5
+#output: 78.57
+#problems: to find area of circle with radius input
+#solution:by using formula (pi*r^2)
+radius = float(input("Enter a number: "))
+pie = 3.14
+area = pie *(radius**2)
+print(f"Area of raduis {int(radius)} is:  {area}")
+
+
