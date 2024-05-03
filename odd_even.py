9bcdf804f78f29ac56734355235636bb3b96ae28
Sisriksha Dhakal <shreedharpandey@Binitas-MacBook-Air.local>
2024-05-03 10:55:18 -0700

    problem 5

---
+++ odd_even.py
@@ -0,0 +1,11 @@
+#input:
+#output:Enter any number to test whether it is even or odd
+#problem:
+#solution:
+num =int(input("Enter a number: "))
+if num %2 ==0:
+    print("Even")
+else:
+    print("Odd")
+
+    
