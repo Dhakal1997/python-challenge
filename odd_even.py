4ab4e61f4daf904e2c1a39a4f03488a354846192
Dhakal1997 <143150030+Dhakal1997@users.noreply.github.com>
2024-08-10 17:06:36 +0545

    Update odd_even.py

--- odd_even.py
+++ odd_even.py
@@ -1,7 +1,7 @@
-#input:
+#input:enetring any numbers
 #output:Enter any number to test whether it is even or odd
-#problem:
-#solution:
+#problem:is the number entered by user is odd or even
+#solution:creating codition that satisfy odd even case
 num =int(input("Enter a number: "))
 if num %2 ==0:
     print("Even")
